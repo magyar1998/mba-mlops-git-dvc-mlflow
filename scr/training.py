55f23860dd9637fef475c5fc604c777e66391d4e
HENRIQUE MAGYAR COSTA <99837631@ab-inbev.com>
2025-06-08 12:15:57 -0300

    Finalizado pipeline modularizada com DVC e MLflow

--- scr/training.py
+++ scr/training.py
@@ -1,12 +1,13 @@
 import numpy as np
 from modulo import simtoseis_library as sts
 import joblib
+import mlflow
 
 #-----------------------------------------------Training-------------------------------------------------------------------------------
 
-prop_treino = 0.75
+prop_treino = 0.70
 n_estimators = 200
-max_depth = 60
+max_depth = 30
 n_jobs = -1
 
 dict_params = {"n_estimators":n_estimators,"max_depth":max_depth, "n_jobs":n_jobs,"proporcao_treino": prop_treino}
@@ -14,8 +15,20 @@ dict_params = {"n_estimators":n_estimators,"max_depth":max_depth, "n_jobs":n_job
 # Carregando dados tratados
 sim_clean = np.load("output/sim_clean.npy")
 
-# Treinando modelo ML
-dados_validacao, y, nrms_teste, r2_teste, mape_teste, modelo, ET, X = sts.ML_model_evaluation(dados_simulacao=sim_clean, modelo="extratrees", dict_params=dict_params)
+
+mlflow.set_tracking_uri('http://127.0.0.1:5000/')
+mlflow.set_experiment(experiment_id=584656037219305060)
+
+with mlflow.start_run():
+
+    mlflow.sklearn.autolog()
+
+    # Treinando modelo ML
+    dados_validacao, y, nrms_teste, r2_teste, mape_teste, modelo, ET, X = sts.ML_model_evaluation(dados_simulacao=sim_clean, modelo="extratrees", dict_params=dict_params)
+
+    dict_metrics = {"parametros": dict_params, "nrms_teste":nrms_teste, "r2_teste":r2_teste}
+
+    mlflow.log_metrics({"nrms_teste": nrms_teste})
 
 # Salvando modelo
 joblib.dump(ET, "output/model.pkl")
